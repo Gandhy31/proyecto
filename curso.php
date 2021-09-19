0b24db95a591c1bf459f5d38d7d3205133107e28
Mateo Granizo <mateo_g13@outlook.com>
2021-09-19 15:16:31 -0500

    Diseño curso

---
+++ curso.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+   <meta charset="utf-8" />
+   <meta name="viewport" content="width=device-width, user-scalable=no" />
+   <title></title>
+   <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
+   <link rel="stylesheet" type="text/css" href="css/style_curso.css" />
+</head>
+
+<body>
+   <header id="header">
+      <div class="wrap">
+
+         <nav class="navbar navbar-expand-lg navbar-dark bg-default ">
+            <div class="container-fluid">
+               <a class="navbar-brand " href="index.php#main-header">Inicio</a>
+               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                  aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+               </button>
+               <div class="collapse navbar-collapse" id="navbarNav">
+
+                  <ul class="navbar-nav">
+
+                     <li class="nav-item ms-3">
+                        <a class="nav-link active " aria-current="page" href="index.php#feature">Acerca de</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link active " aria-current="page" href="index.php#service">Servicios</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link active " aria-current="page" href="index.php#portfolio">Cursos</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link active " aria-current="page" href="index.php#testimonial">Testimonios</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link active " aria-current="page" href="index.php#blog">Instructores</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link active " aria-current="page" href="index.php#contact">Contacto</a>
+                     </li>
+
+                  </ul>
+               </div>
+            </div>
+         </nav>
+
+      </div>
+   </header>
+
+   <section id="info" class="wrap">
+      <div id="imagen"><img src="img/c1.jpg"></div>
+      <div id="detalle">
+         <h2>CURSO DE MAQUILLAJE PROFESIONAL ONLINE</h2>
+         <p>Estudia tu carrera de Maquillaje Profesional en la modalidad online y aprende a través de módulos, videos,
+            demostraciones y prácticas dirigidas. Brindamos todos los conocimientos básicos y avanzados para que puedas
+            desarrollarte en la industria del maquillaje profesional. <br>
+
+            Nuestro curso online tiene el mismo contenido que el curso presencial con el propósito de asegurarnos que
+            todos nuestros estudiantes adquieran el mismo nivel de educación.
+         </p>
+      </div>
+      <div class="clearfix"></div>
+
+      <div id="datos">
+
+         <div class="titulos costo">
+            <h1> <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                  class="bi bi-cash me-2 mb-1" viewBox="0 0 16 16">
+                  <path d="M8 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
+                  <path
+                     d="M0 4a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V4zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V6a2 2 0 0 1-2-2H3z" />
+               </svg>Costos</h1>
+            <p class="ms-4">$280</p>
+         </div>
+
+         <div class="titulos nivel">
+            <h1> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  class="bi bi-bar-chart-steps me-2 mb-1" viewBox="0 0 16 16">
+                  <path
+                     d="M.5 0a.5.5 0 0 1 .5.5v15a.5.5 0 0 1-1 0V.5A.5.5 0 0 1 .5 0zM2 1.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-4a.5.5 0 0 1-.5-.5v-1zm2 4a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-1zm2 4a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-6a.5.5 0 0 1-.5-.5v-1zm2 4a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-1z" />
+               </svg>Nivel</h1>
+            <p class="ms-4">Básico - avanzado</p>
+         </div>
+
+         <div class="titulos duracion">
+            <h1><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  class="bi bi-hourglass-split me-2 mb-1" viewBox="0 0 16 16">
+                  <path
+                     d="M2.5 15a.5.5 0 1 1 0-1h1v-1a4.5 4.5 0 0 1 2.557-4.06c.29-.139.443-.377.443-.59v-.7c0-.213-.154-.451-.443-.59A4.5 4.5 0 0 1 3.5 3V2h-1a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-1v1a4.5 4.5 0 0 1-2.557 4.06c-.29.139-.443.377-.443.59v.7c0 .213.154.451.443.59A4.5 4.5 0 0 1 12.5 13v1h1a.5.5 0 0 1 0 1h-11zm2-13v1c0 .537.12 1.045.337 1.5h6.326c.216-.455.337-.963.337-1.5V2h-7zm3 6.35c0 .701-.478 1.236-1.011 1.492A3.5 3.5 0 0 0 4.5 13s.866-1.299 3-1.48V8.35zm1 0v3.17c2.134.181 3 1.48 3 1.48a3.5 3.5 0 0 0-1.989-3.158C8.978 9.586 8.5 9.052 8.5 8.351z" />
+               </svg>Duración</h1>
+            <p class="ms-4">3 meses intensivos. <br> Completa 2 módulos semanales para poder avanzar con las siguientes
+               clases de tu curso.</p>
+         </div>
+
+         <div class="titulos fecha_inicio">
+            <h1> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  class="bi bi-calendar-event me-2 mb-1" viewBox="0 0 16 16">
+                  <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z" />
+                  <path
+                     d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+               </svg>Fecha de inicio</h1>
+            <p class="ms-4">26 de abril 2021</p>
+         </div>
+
+         <div class="titulos certificado">
+            <h1> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  class="bi bi-patch-check-fill me-2 mb-1" viewBox="0 0 16 16">
+                  <path
+                     d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" />
+               </svg>Certificado</h1>
+            <p class="ms-4">Las personas que culminen satisfactoriamente el curso recibirán un certificado avalado por
+               la escuela Inua y por el Ministerio de Trabajo.</p>
+         </div>
+
+      </div>
+
+      <div id="acordeon">
+
+         <div class="accordion" id="accordionExample">
+
+            <div class="accordion-item">
+               <h2 class="accordion-header" id="headingOne">
+                  <button class="accordion-button collapsed bg-light" type="button" data-bs-toggle="collapse"
+                     data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                     INICIACIÓN AL MAQUILLAJE PROFESIONAL
+                  </button>
+               </h2>
+               <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
+                  data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                     <ul>
+                        <li>Fundamentos del maquillaje</li>
+                        <li>Normas del maquillador</li>
+                        <li>Lenguaje técnico</li>
+                        <li>Herramientas del maquillador</li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+
+            <div class="accordion-item">
+               <h2 class="accordion-header" id="headingTwo">
+                  <button class="accordion-button collapsed bg-transparent" type="button" data-bs-toggle="collapse"
+                     data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                     PIEL
+                  </button>
+               </h2>
+               <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+                  data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                     <ul>
+                        <li>Anatomía y patologías de la piel</li>
+                        <li>Tipos de piel</li>
+                        <li>Preparación de piel</li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+
+            <div class="accordion-item">
+               <h2 class="accordion-header" id="headingThree">
+                  <button class="accordion-button collapsed bg-transparent" type="button" data-bs-toggle="collapse"
+                     data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                     CORRECCIONES CROMÁTICAS Y MOFOLÓGICAS
+                  </button>
+               </h2>
+               <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
+                  data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                     <ul>
+                        <li>Visagismo</li>
+                        <li>Correctores</li>
+                        <li>Bases y primers</li>
+                        <li>Iluminación y contornos</li>
+                        <li>Teoría del calor</li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+
+            <div class="accordion-item">
+               <h2 class="accordion-header" id="headingFour">
+                  <button class="accordion-button collapsed bg-transparent" type="button" data-bs-toggle="collapse"
+                     data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                     TÉCNICAS DE MAQUILLAJE 1
+                  </button>
+               </h2>
+               <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour"
+                  data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                     <ul>
+                        <li>Partes del ojo</li>
+                        <li>Diseño de cejas</li>
+                        <li>Protocolos del maquillaje</li>
+                        <li>Maquillaje nude/soft</li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+
+            <div class="accordion-item">
+               <h2 class="accordion-header" id="headingFive">
+                  <button class="accordion-button collapsed bg-transparent" type="button" data-bs-toggle="collapse"
+                     data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                     TÉCNICAS DE MAQUILLAJE 2
+                  </button>
+               </h2>
+               <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive"
+                  data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                     <ul>
+                        <li>Delineados</li>
+                        <li>Maquillaje pin up</li>
+                        <li>Maquillaje para piel madura</li>
+                        <li>Maquillaje para reina</li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+
+            <div class="accordion-item">
+               <h2 class="accordion-header" id="headingSix">
+                  <button class="accordion-button collapsed bg-transparent" type="button" data-bs-toggle="collapse"
+                     data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                     TÉCNICAS DE MAQUILLAJE 3
+                  </button>
+               </h2>
+               <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix"
+                  data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                     <ul>
+                        <li>Maquillaje para quinceañera</li>
+                        <li>Maquillaje de novia glam clásica</li>
+                        <li>Maquillaje de novia glam hd</li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+
+            <div class="accordion-item">
+               <h2 class="accordion-header" id="headingSeven">
+                  <button class="accordion-button collapsed bg-transparent" type="button" data-bs-toggle="collapse"
+                     data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+                     TÉCNICAS DE MAQUILLAJE 4
+                  </button>
+               </h2>
+               <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingSeven"
+                  data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                     <ul>
+                        <li>Maquillaje para piel morena</li>
+                        <li>Maquillaje editorial/artistico</li>
+                        <li>Proyecto maquillaje editorial</li>
+                        <li>Análisis de productos</li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+
+            <div class="accordion-item">
+               <h2 class="accordion-header" id="headingEight">
+                  <button class="accordion-button collapsed bg-transparent" type="button" data-bs-toggle="collapse"
+                     data-bs-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+                     MODELO DE NEGOCIO
+                  </button>
+               </h2>
+               <div id="collapseEight" class="accordion-collapse collapse" aria-labelledby="headingEight"
+                  data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                     <ul>
+                        <li>Definición de segmento</li>
+                        <li>Preposición de valor</li>
+                        <li>Marketing digital</li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+
+
+
+         </div>
+      </div>
+
+
+   </section>
+
+   <script type="text/javascript" src="jquery/jquery-3.6.0.min.js"></script>
+   <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>

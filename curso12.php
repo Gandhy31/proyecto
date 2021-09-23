d2ac71fccc7cc7dac74ec4fb426b4376876ba453
gandhy garcia <gandhy.garcia@unach.edu.ec>
2021-09-23 15:20:53 -0500

    cambios

--- curso12.php
+++ curso12.php
@@ -81,7 +81,7 @@ $row2 = mysqli_fetch_array($query2);
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-circle me-1 mb-1" viewBox="0 0 16 16">
                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                        </svg>Matt
+                        </svg><?php echo $row['nombres']." ".$row['apellidos'] ?>
                      </a>
 
                      <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuLink">

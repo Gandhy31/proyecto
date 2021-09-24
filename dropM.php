a2a2470e59710c613f5add44f5bdd9a9a81b3b5f
gandhy garcia <gandhy.garcia@unach.edu.ec>
2021-09-24 13:22:19 -0500

    cambios

--- dropM.php
+++ dropM.php
@@ -2,13 +2,14 @@
     include("conexion.php");
     $conn = conectar();
     $id = $_GET['id'];
+    $idAd = $_GET['idAd'];
     $sql = "DELETE 
             FROM mensaje 
             WHERE id='$id'";
     $squery=mysqli_query($conn, $sql);
     if($squery){
 
-        Header("Location: admin1.php");
+        Header("Location: admin1.php?id=$idAd");
 
     }
 ?>

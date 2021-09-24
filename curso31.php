7208eece6fc6747f9bc2a3eafda272c934c42a41
Mateo Granizo <mateo_g13@outlook.com>
2021-09-23 20:08:48 -0500

    Curso 3 añadido

--- curso31.php
+++ curso31.php
@@ -1,9 +1,30 @@
 <?php
 include("conexion.php");
 $conn = conectar();
+session_start();
+if (!isset($_SESSION['idG'])) {
+   header("Location: index.php");
+}
+$id = $_GET['id'];
+$sql = "SELECT*
+          FROM usuario
+          WHERE id='$id'";
+$query = mysqli_query($conn, $sql);
+$row = mysqli_fetch_array($query);
+$cont = 0;
+$sql1 = "SELECT*
+           FROM factura
+           WHERE idUsuario='$id' AND idCurso=1";
+$query1 = mysqli_query($conn, $sql1);
+$comp = mysqli_num_rows($query1);
+$row1 = mysqli_fetch_array($query1);
+if ($comp >= 1) {
+   $cont = 1;
+   $ac = $row1['activo'];
+}
 $sql2 = "SELECT*
            FROM curso
-           WHERE id=2";
+           WHERE id=3";
 $query2 = mysqli_query($conn, $sql2);
 $row2 = mysqli_fetch_array($query2);
 ?>
@@ -54,10 +75,23 @@ $row2 = mysqli_fetch_array($query2);
 
                   </ul>
 
-                  <ul class="navbar-nav ms-auto">
-                     <li class="nav-item"><a class="nav-link active " aria-current="page" href="login.php">Iniciar Sesión</a></li>
-                     <li class="nav-item"><a class="nav-link active " aria-current="page" href="registro.php">Registrarse</a></li>
-                  </ul>
+                  <div class="dropdown ms-auto">
+                     <a class="btn btn-secondary dropdown-toggle avatar bg-transparent border-0 " href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-circle me-1 mb-1" viewBox="0 0 16 16">
+                           <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                           <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                        </svg><?php echo $row['nombres']." ".$row['apellidos'] ?>
+                     </a>
+
+                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">Iniciado como <strong><?php echo $row['usuario'] ?></strong></a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="usuario.php?id=<?php echo $row['id']?>">Editar perfil</a></li>
+                        <li><a class="dropdown-item" href="historial.php?id=<?php echo $row['id']?>">Historial de compras</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="logout.php">Cerrar sesión</a></li>
+                     </ul>
+                  </div>
 
                </div>
             </div>
@@ -67,15 +101,15 @@ $row2 = mysqli_fetch_array($query2);
    </header>
 
    <section id="info" class="wrap">
-      <div id="imagen"><img src="img/c2.jpg" width="635px" height="506px"></div>
+      <div id="imagen"><img src="img/c3.png" width="635px" height="506px"></div>
       <div id="detalle">
-         <h2>CURSO DE AUTOMAQUILLAJE COMPLETO</h2>
-         <p>En este curso conocerás la teoría fundamental del maquillaje y aprenderás a maquillarte con técnica profesional
-            a través de clases en videos full HD, guías de apoyo y hojas de práctica.
+         <h2>CURSO TÉCNICA ESPECIALISTA EN UÑAS</h2>
+         <p>
+            En este curso aprenderás las mejores técnicas y variedad de diseños para convertirte en una técnica especialista en uñas.
             <br>
-            Con este curso aprenderás a descubrir la mejor versión de tí. Es importante que sepas reconocer tu tipo de piel, rostro y rasgos morfólógicos para que puedas aplicar los productos correctos con la técnica correcta.
+            Iniciamos desde cero, no se necesitan conocimientos previos, solo muchas ganas de aprender.
             <br>
-            Este es un curso práctico que te permitirá, de manera rápida, obtener resultados profesionales en tu maquillaje.
+            Esta profesión es una de las que más demanda tienen y te permiten desarrollar tu propio negocio o trabajar en un centro de belleza inmediatamente.
          </p>
       </div>
       <div class="clearfix"></div>
@@ -87,21 +121,21 @@ $row2 = mysqli_fetch_array($query2);
                   <path d="M8 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
                   <path d="M0 4a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V4zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V6a2 2 0 0 1-2-2H3z" />
                </svg>Costos</h1>
-            <p class="ms-4"><?php echo "$ " . $row2['precio'] ?></p>
+            <p class="ms-4">Mensual <?php echo "$ " . $row2['precio'] ?></p>
          </div>
 
          <div class="titulos nivel">
             <h1> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bar-chart-steps me-2 mb-1" viewBox="0 0 16 16">
                   <path d="M.5 0a.5.5 0 0 1 .5.5v15a.5.5 0 0 1-1 0V.5A.5.5 0 0 1 .5 0zM2 1.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-4a.5.5 0 0 1-.5-.5v-1zm2 4a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-1zm2 4a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-6a.5.5 0 0 1-.5-.5v-1zm2 4a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-1z" />
                </svg>Nivel</h1>
-            <p class="ms-4">Básico</p>
+            <p class="ms-4">Básico - avanzado</p>
          </div>
 
          <div class="titulos duracion">
             <h1><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hourglass-split me-2 mb-1" viewBox="0 0 16 16">
                   <path d="M2.5 15a.5.5 0 1 1 0-1h1v-1a4.5 4.5 0 0 1 2.557-4.06c.29-.139.443-.377.443-.59v-.7c0-.213-.154-.451-.443-.59A4.5 4.5 0 0 1 3.5 3V2h-1a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-1v1a4.5 4.5 0 0 1-2.557 4.06c-.29.139-.443.377-.443.59v.7c0 .213.154.451.443.59A4.5 4.5 0 0 1 12.5 13v1h1a.5.5 0 0 1 0 1h-11zm2-13v1c0 .537.12 1.045.337 1.5h6.326c.216-.455.337-.963.337-1.5V2h-7zm3 6.35c0 .701-.478 1.236-1.011 1.492A3.5 3.5 0 0 0 4.5 13s.866-1.299 3-1.48V8.35zm1 0v3.17c2.134.181 3 1.48 3 1.48a3.5 3.5 0 0 0-1.989-3.158C8.978 9.586 8.5 9.052 8.5 8.351z" />
                </svg>Duración</h1>
-            <p class="ms-4">3 horas intensivas</p>
+            <p class="ms-4">3 meses</p>
          </div>
 
          <div class="titulos fecha_inicio">
@@ -109,7 +143,11 @@ $row2 = mysqli_fetch_array($query2);
                   <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z" />
                   <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
                </svg>Fecha de inicio</h1>
-            <p class="ms-4">Disponible en cualquier momento</p>
+            <p class="ms-4">
+               Miércoles de 09h00 a 12h00
+               <br>
+               Domingos de 09h30 a 12h30
+            </p>
          </div>
 
          <div class="titulos certificado">
@@ -117,25 +155,112 @@ $row2 = mysqli_fetch_array($query2);
                   <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" />
                </svg>Certificado</h1>
             <p class="ms-4">Las personas que culminen satisfactoriamente el curso recibirán un certificado avalado por
-               la escuela Inua </p>
+               la escuela Inua y la Asociación Interprofesional de Artesanos</p>
          </div>
 
       </div>
 
       <div id="acordeon">
 
-         <ul class="list-group list-group-flush">
-            <li id="list-group" class="list-group-item bg-ligth">RECONOCE TU TIPO DE PIEL</li>
-            <li id="list-group" class="list-group-item bg-ligth">REOCNOCE TU TIPO DE ROSTRO</li>
-            <li id="list-group" class="list-group-item bg-light">IDENTIFICA LOS PRODUCTOS QUE DEBES USAR Y LOS QUE NO DEBES UTILIZAR</li>
-            <li id="list-group" class="list-group-item bg-light">CONOCE CÓMO UTILIZAR LA TEORÍA DEL COLOR PAR AQUE TU MAQUILLAJE TENGA ARMONÍA</li>
-            <li id="list-group" class="list-group-item bg-light">APRENDE TÉCNICA DE MAQUILLAJE PROFESIONAL DE FORMA RÁPIDA Y PRÁCTICA</li>
-            <li id="list-group" class="list-group-item bg-light">APRENDE UN LOOK DE DÍA Y CÓMO TRANSFORMAR ESTE EN UN MAQUILLAJE DE NOCHE</li>
-         </ul>
+         <div class="accordion" id="accordionExample">
+
+            <div class="accordion-item">
+               <h2 class="accordion-header" id="headingOne">
+                  <button class="accordion-button collapsed bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                     MÓDULO 1
+                  </button>
+               </h2>
+               <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                     <ul>
+                        <li>Anatomía de la uña</li>
+                        <li>Cuidados y patologías de la uña</li>
+                        <li>Técnicas de bioseguridad</li>
+                        <li>Protocolos de servicio</li>
+                        <li>Manicura rusa y manejo de drill</li>
+                        <li>Puntas vanguardistas</li>
+                        <li>Técnicas de limado y tipos de limas</li>
+                        <li>Estructura</li>
+                        <li>Acrílico nivel 1</li>
+                        <li>Gel y acrigel nivel 1</li>
+                        <li>3D nivel 1 y 2</li>
+                        <li>Baby Boomer y técnica ombré</li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+
+            <div class="accordion-item">
+               <h2 class="accordion-header" id="headingTwo">
+                  <button class="accordion-button collapsed bg-transparent" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                     MÓDULO 2
+                  </button>
+               </h2>
+               <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                     <ul>
+                        <li>Esculpido</li>
+                        <li>Polygel nivel 1 y 2</li>
+                        <li>Esmaltado semipermanente</li>
+                        <li>Técnica navaja</li>
+                        <li>Técnica efecto marmol</li>
+                        <li>Degradé de colores</li>
+                        <li>Encapsulado con flores o naturaleza muerta</li>
+                        <li>Efecto sugar</li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+
+            <div class="accordion-item">
+               <h2 class="accordion-header" id="headingThree">
+                  <button class="accordion-button collapsed bg-transparent" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                     MÓDULO 3
+                  </button>
+               </h2>
+               <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                     <ul>
+                        <li>Painting gel efecto jersey</li>
+                        <li>Painting gel efecto arcoiris</li>
+                        <li>Blooming gel y ojo de gato</li>
+                        <li>Efecto espejo, unicornio aurora y holográfico</li>
+                        <li>Spider gel, cola de sirena y efecto foil</li>
+                        <li>Diseño de pigmento y colorimetría</li>
+                        <li>Stamping uñas mágicas y efecto repujado</li>
+                        <li>Diseño de flores gel plastilina 4D</li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+
+         </div>
 
       </div>
 
-      <button type="button" class="btn btn-success btn-lg mt-3 ms-auto">Comprar</button>
+      <a id="btn1" class="btn btn-success btn-lg mt-3 ms-auto
+         <?php
+         if ($cont == 1) {
+            echo "disabled";
+         }
+
+         ?>" href="compra.php?id=<?php echo $row['id'] ?>&idc=1" onClick="location.href: 'https://wa.me/593980264931?text=Me%20interesa%20el%20Curso%20de%20Maquillaje%20Profesional%20Online'" role="button" <?php
+                                                                                                                                                                                                               if ($cont == 1) {
+                                                                                                                                                                                                                  echo "aria-disabled=\"true\" ";
+                                                                                                                                                                                                               }
+                                                                                                                                                                                                               ?>>
+         <?php
+         if ($cont == 1) {
+            if ($ac == 0) {
+               echo "Pago pendiente";
+            } else {
+               echo "Comprado";
+            }
+         } else {
+            echo "Comprar";
+         }
+         ?>
+      </a>
 
    </section>
 

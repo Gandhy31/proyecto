2dd2f780eede43a2cef55a362e003dcd848e3ec0
gandhy garcia <gandhy.garcia@unach.edu.ec>
2021-09-27 10:52:37 -0500

    final

--- registro.php
+++ registro.php
@@ -11,7 +11,7 @@ if (isset($_SESSION['idG'])) {
 }
 ?>
 <!doctype html>
-<html lang="en">
+<html lang="es">
 
 <head>
   <!-- Required meta tags -->
--- registro2.php
+++ registro2.php
@@ -23,7 +23,7 @@
 ?>
 
 <!doctype html>
-<html lang="en">
+<html lang="es">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">

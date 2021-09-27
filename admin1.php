cb12bd8ac2354e351f3286cd0b73a8e2ea1f6813
gandhy garcia <gandhy.garcia@unach.edu.ec>
2021-09-26 23:42:43 -0500

    mensa

--- admin1.php
+++ admin1.php
@@ -80,11 +80,11 @@
       <div class="accordion col-md-12" id="accordionExample">
          <div class="accordion-item">
             <h2 class="accordion-header" id="headingOne">
-               <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+               <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                Mensajes
                </button>
             </h2>
-            <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                <div class="accordion-body">  
                   <table class="table">
                      <thead>
